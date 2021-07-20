599aea979a8b1286041f3db7a039da8aef8a149a
Samagra10 <divyashukla1500@gmail.com>
2021-07-20 05:55:11 +0530

    my first commmit finalproject

--- src/Four.jsx
+++ src/Four.jsx
@@ -5,11 +5,12 @@ function Four()
 
     return(
     <>
+   
     <img src="http://ksnidankendra.com/downloads/2894ph2.jpg" width="30%" height="180px;"></img>
-    <p id="four">Dr. Divya Shukla</p>
-    <p id="five">Certified gynecologist</p>
-    <p id="six">Dr. Divya Shukla is on of the leading practicionists in the city with a private hospital and many facilities for patients</p>
-    <p id="six">Schedule a consultation:9554513225</p>
+    <p className="special"  id="four">Dr. Divya Shukla</p>
+    <p className="special" id="five">Certified gynecologist</p>
+    <p className="special" id="six">Dr. Divya Shukla is on of the leading practicionists in the city with a private hospital and many facilities for patients</p>
+    <p className="special" id="six">Schedule a consultation:9554513225</p>
     </>
     );
     
--- src/Six.jsx
+++ src/Six.jsx
@@ -4,17 +4,17 @@ function Six()
 {
     return(
         <>
-    <div className="jumbotron">
-    <div className="card-deck">
-  <div className="card bg-dark">
+        <div className="card-deck " >
+     <div className="card" className="samagra4">
     <div className="card-body text-white text-center">
-      <p className="card-text">Gorakhpur</p>
+    <p className="card-text">Gorakhpur</p>
       <p className="card-text">Kasia Road,OPP. of</p>
       <p className="card-text">Indian Oil petrol pump,</p>
       <p className="card-text">U.P., India.</p>
     </div>
   </div>
-  <div className="card bg-dark">
+      
+  <div className="card" className="samagra4">
     <div className="card-body text-white text-center">
     <p className="card-text">Contact</p>
       <p className="card-text">Contact:9554513225</p>
@@ -23,7 +23,6 @@ function Six()
     </div>
   </div>
   </div>
-    </div>
         </>
     );
 };
